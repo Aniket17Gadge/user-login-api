8d1cf515666fbec04c4803a1fdfa1bb99dc78d78
Aniket17Gadge <aniketgadge17@gmail.com>
2025-07-22 18:28:01 +0530

    JWT configuration Done

--- login_api/login_api/settings.py
+++ login_api/login_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auth_app'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
