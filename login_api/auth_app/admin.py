d461adaf16d85f156b7272bf3476b6723639c2a4
Aniket17Gadge <aniketgadge17@gmail.com>
2025-07-23 02:34:01 +0530

    Adding comments to understand the code

--- login_api/auth_app/admin.py
+++ login_api/auth_app/admin.py
@@ -8,11 +8,14 @@ from django import forms
 from django.contrib import admin
 from .models import RegisteredEmail, OTP
 
+#Useful for tracking which emails are registered in the system.
 @admin.register(RegisteredEmail)
 class RegisteredEmailAdmin(admin.ModelAdmin):
-    list_display = ['email']
-    search_fields = ['email']
+    list_display = ['email'] #Shows email field in list view.
 
+    search_fields = ['email']#Allows search by email.
+
+#monitor OTPs being generated 
 @admin.register(OTP)
 class OTPAdmin(admin.ModelAdmin):
     list_display = ['email', 'hashed_otp', 'salt', 'created_at']
